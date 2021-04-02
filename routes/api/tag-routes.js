e4abd630591c29c416d4bd6ecf27585c308b5368
dboren <danielboren@att.net>
2021-04-02 15:11:01 -0500

    added route to get all tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,9 +3,17 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  try {
+    const tagData = await tag.findAll({
+      include: [{model: Product}]
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
