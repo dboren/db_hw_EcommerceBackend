594171cb4dd302a413ec604733ae5e38b5d03d66
dboren <danielboren@att.net>
2021-04-03 18:37:54 -0500

    added ReadMe

--- README_tbe.md
+++ README_tbe.md
@@ -1,2 +1,28 @@
-# db_hw_EcommerceBackend
-Repo for e-commerce assignment
+# E-commerce Backend
+
+
+## Value Add Proposal
+
+App allows user to create and seed a database of products, categories, and tags, and to view, add to, update, and delete from each of those tables via api routes.
+
+
+## Tech Stack
+
+Application demonstrates ability with javascript, mysql, Sequelize, node, and express.
+
+
+## MVP
+
+### Phase One
+
+Server and connection files were configured and models were created for the tables product, category, tag, and product tag. This is then used along with starter sql files to create and seed the e-commerce database.
+
+
+### Phase Two
+
+API routes are created for the three primary tables to allow use of the get, post, put, and delete http requests. All routes added use async/await format and sequelize methods.
+
+
+## Demo
+
+
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -44,12 +44,38 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      }
+    });
+    if (!tagData[0]) {
+      res.status(404).json({ message: 'no tag matches this ID' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.destroy( {
+      where: {
+        id: req.params.id,
+      }
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'no tag matches this ID' });
+      return;
+    } 
+    res.status(200).json(tagData);
+   } catch (err) {
+     res.status(500).json(err);
+   }
 });
 
 module.exports = router;
