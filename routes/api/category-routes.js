7270bf6caa3fe8b125f4ecd7d44c927f170bb25d
dboren <danielboren@att.net>
2021-04-03 17:15:49 -0500

    added route to get tag by ID. Confirmed working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -30,7 +30,7 @@ router.get('/:id', async (req, res) => {
     });
 
     if (!categoryData) {
-      res.status(404).json({ message: 'No category matches this id' });
+      res.status(404).json({ message: 'No category matches this ID' });
       return;
     }
 
@@ -59,7 +59,7 @@ router.put('/:id', async (req, res) => {
       }
     });
     if (!categoryData[0]) {
-      res.status(404).json( { message: 'No category matches this id' } );
+      res.status(404).json( { message: 'No category matches this ID' } );
       return;
     }
     res.status(200).json(categoryData);
@@ -77,7 +77,7 @@ router.delete('/:id', async (req, res) => {
       }
     });
     if (!categoryData) {
-      res.status(404).json({ message: 'no category matches this id' });
+      res.status(404).json({ message: 'no category matches this ID' });
       return;
     } 
     res.status(200).json(categoryData);
